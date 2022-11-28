25f26f459c424575580115d2ab05512d10d705be
tugas_besar_PPPB <juliakirani27@gmail.com>
2022-11-28 14:39:12 +0700

    tugas_besar_PPPB

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Pages
+import LandingPage from "./pages/LandingPage";
+// Components
+import Header from "./components/Header";
+import "./App.css";
+function App() {
+return (
+<div className="App">
+<Header />
+<Router>
+<Routes>
+<Route exact path="/" element={<LandingPage/>} />
+</Routes>
+</Router>
+</div>
+);
+}
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+// index.js
+import React from "react";
+import "./index.css";
+export default function index() {
+ return (
+ <header>
+ <h3>Kelompok 22</h3>
+ <p>Modul 6 - PWA 2</p>
+ </header>
+ );
+}
